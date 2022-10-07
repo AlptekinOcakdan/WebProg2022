d10428c09ad4a5d7f883bd1d79a8a63b125c60e9
Alptekin <alptekin.ocakdan1@ogr.sakarya.edu.tr>
2022-10-07 16:06:05 +0300

    First Lesson
    He showed how we control the MVC Structure.

---
+++ Lesson1/Controllers/OgrenciController.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Lesson1.Controllers
+{
+    public class OgrenciController:Controller
+    {
+        public string Ahmet()
+        {
+
+            return "Ahmet Ses Verdi";
+        }
+    }
+}
---
+++ Lesson1/Controllers/PersonelController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Lesson1.Controllers
+{
+    public class PersonelController : Controller
+    {
+        public IActionResult Goster()
+        {
+            return View();
+        }
+    }
+}
