b151dbefd4243490abd7df8915ff2a27468abd8a
Alptekin <alptekin.ocakdan1@ogr.sakarya.edu.tr>
2022-10-12 09:13:04 +0300

    blah blah

---
+++ Lesson2/Controllers/OgrenciController.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.VisualBasic;
+
+namespace Lesson2.Controllers
+{
+    public class OgrenciController : Controller
+    {
+        public IActionResult OgrenciEkle()
+        {
+            return View();
+        }
+
+        public string OgrKaydet(string fname, string lname, string Onum)
+        {
+            string Msg = fname + lname + Onum;
+            return Msg;
+        }
+    }
+}
