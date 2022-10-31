dd47b3d3f0d7d1b3eddc58ee173542d1164539c7
Alptekin Ocakdan <alptekin.ocakdan@gmail.com>
2022-10-31 18:07:46 +0300

    Viewbag usage, Homework

---
+++ Lesson4/Controllers/OgrenciController.cs
@@ -0,0 +1,31 @@
+﻿using Lesson4.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+
+namespace Lesson4.Controllers
+{
+    public class OgrenciController : Controller
+    {
+        static List<Ogrenci> ogrenciler=new List<Ogrenci>();
+        public IActionResult Index()
+        {
+            ViewBag.SehirKod = new List<SelectListItem>
+            {
+                new SelectListItem { Value = "06", Text = "Ankara" },
+                new SelectListItem { Value = "34", Text = "İstanbul" },
+                new SelectListItem { Value = "35", Text = "İzmir" },
+                new SelectListItem { Value = "54", Text = "Sakarya" },
+            };
+            return View();
+        }
+        public IActionResult Index2()
+        {
+            return View();
+        }
+
+        public IActionResult OgrKaydet(Ogrenci ogrenci)
+        {
+            return View();
+        }
+    }
+}
---
+++ Lesson4/Models/Ogrenci.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Lesson4.Models
+{
+    public class Ogrenci
+    {
+        [Display(Name = "Öğrenci Ad")]
+        [Required(ErrorMessage = "Alanı Doldurunuz.")]
+        public string OgrAd { get; set; }
+        [Display(Name = "Öğrenci Soyad")]
+        public string OgrSoyad { get; set; }
+        [Display(Name = "Öğrenci No")]
+        public string OgrNo { get; set; }
+        [Display(Name = "Öğrenci Yaşı")]
+        public int OgrYas { get; set; }
+        [Display(Name = "Şehirler")]
+        public string Sehirler { get; set; }
+    }
+}
